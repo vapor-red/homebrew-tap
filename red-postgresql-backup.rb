53a30035c37a64adbae455294b7466e75f82c455
Jonas Schwartz <jonas@qutheory.io>
2019-02-25 02:50:37 +0100

    Init

---
+++ red-postgresql-backup.rb
@@ -0,0 +1,12 @@
+class RedPostgresqlBackup < Formula
+  desc "Backup Vapor Red PostgreSQL databases"
+  url "https://red-scripts.ams3.digitaloceanspaces.com/postgresql-backup-0.0.1.tar.gz"
+  sha256 "e4b7685fd2913b65bee80b1d85227cce2129bd545c7fc06bd486ac5285c1167a"
+  version "0.0.1"
+
+  def install
+    libexec.install Dir["*"]
+    bin.write_exec_script (libexec/"psql-backup")
+    bin.write_exec_script (libexec/"psql-backup-setup")
+  end
+end
