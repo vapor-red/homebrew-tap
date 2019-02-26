8b2a1fd8265dcd5d08aa8b5307b2491dd81c995f
Jonas Schwartz <jonas@qutheory.io>
2019-02-26 14:20:53 +0100

    Adding red-backup 0.0.1

---
+++ red-backup.rb
@@ -0,0 +1,11 @@
+class RedBackup < Formula
+  desc "Backup Vapor Red"
+  url "https://red-scripts.ams3.digitaloceanspaces.com/red-backup-0.0.1.tar.gz"
+  sha256 "e1562783c8ca824df3bfe921fb891ac13117fe606590de38a667ad7dcb1ec533"
+  version "0.0.1"
+
+  def install
+    libexec.install Dir["*"]
+    bin.write_exec_script (libexec/"red-backup")
+  end
+end
