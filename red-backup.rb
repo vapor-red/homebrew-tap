e940c3a0cd81f11b3e2ef9e8d1e679ea067c3353
Jonas Schwartz <jonas@qutheory.io>
2019-02-26 14:57:49 +0100

    Adding red-backup 0.0.1

--- red-backup.rb
+++ red-backup.rb
@@ -15,7 +15,25 @@ class RedBackup < Formula
   version "0.0.1"
 
   def install
+    (lib/"red-backup/vendor").mkpath
+    resources.each do |r|
+      r.verify_download_integrity(r.fetch)
+      system("gem", "install", r.cached_download, "--no-documentation", "--install-dir", "#{lib}/red-backup/vendor")
+    end
+
+    rm_rf "vendor"
+    (bin/"red-backup").write(exec_script)
+
     libexec.install Dir["*"]
     bin.write_exec_script (libexec/"red-backup")
+  end
+
+  def exec_script
+    <<-EOS.undent
+    #!/bin/bash
+    export GEM_HOME="#{HOMEBREW_PREFIX}/lib/red-backup/vendor"
+    export DISABLE_BUNDLER_SETUP=1
+    exec ruby "#{libexec}/red-backup.rb" "$@"
+    EOS
   end
 end
