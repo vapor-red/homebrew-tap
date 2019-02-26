284d6968cb5db0d8e9b690afb8fd6b41d6a0e18e
Jonas Schwartz <jonas@qutheory.io>
2019-02-26 14:28:06 +0100

    Adding red-backup 0.0.1

--- red-backup.rb
+++ red-backup.rb
@@ -1,4 +1,7 @@
 class RedBackup < Formula
+  depends_on 'colorize' => :ruby
+  depends_on 'tty-prompt' => :ruby
+
   desc "Backup Vapor Red"
   url "https://red-scripts.ams3.digitaloceanspaces.com/red-backup-0.0.1.tar.gz"
   sha256 "e1562783c8ca824df3bfe921fb891ac13117fe606590de38a667ad7dcb1ec533"
